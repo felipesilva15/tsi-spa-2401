c59eeefbf4b3cff8229d34c652d25f5e959acb28
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2024-03-08 21:45:16 -0300

    feat: Melhorando o código da livraria

--- livraria-senac/src/components/mostSearched/MostSearched.jsx
+++ livraria-senac/src/components/mostSearched/MostSearched.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react"
 import { List, ListItem } from "../styles/Lists";
 
+const baseUrl = 'localhost:8000'
+
 function MostSearched() {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
         const getBooks = async () => {
             try {
-                const res = fetch('localhost:8000/books');
+                const res = fetch(`${baseUrl}/books`);
                 const data = await res.json();
 
                 setBooks(data);
@@ -15,6 +17,8 @@ function MostSearched() {
                 console.error(error);
             }
         };
+
+        getBooks();
     }, books);
 
     return (
